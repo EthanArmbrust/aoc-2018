62056db5325e85f563bd59e8c0c9ff73752469ef
EthanArmbrust <ethanarm@buffalo.edu>
2019-09-19 12:29:52 -0400

    back to 2018

---
+++ aoc4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct guard{
+    int id;
+    vector<pair<int,int>> sleep_times;
+}
+
+int main(){
+
+    ifstream infile("input/4.txt");
+    string s;
+    while(getline(infile, s)){
+        sscanf(s.c_str(), "
+    }  
+
+
+
+    return 0;
+}
