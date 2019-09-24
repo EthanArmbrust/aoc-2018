1931af562f9664df0c1d6733da4e5f47ab4b71c5
EthanArmbrust <ethanarm@buffalo.edu>
2019-09-24 12:17:22 -0400

    day 7 part 2 is so close. I can taste it

--- aoc7-2.cpp
+++ aoc7-2.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+struct elfo{
+    char current_job = '.'; //'.' means not working
+    int job_start;
+};
+
 int main(){
 
     ifstream infile("input/7.txt");
@@ -18,10 +23,13 @@ int main(){
     int f_index = 5;
     int s_index = 36;
 
+    int bonus_time = 60;
+
     vector<string> deps(26, "");
+    vector<elfo> elves(5);
+    vector<char> job_queue;
 
     for(auto str : inputList){
-        dep[str[s_index]] += str[f_index];
         deps[str[s_index] - 'A'] += str[f_index];
     }
 
@@ -34,37 +42,50 @@ int main(){
     for(int k = 0; k < uncompleted.size(); k++){
         char key = uncompleted[k];
         if(deps[int(key - 'A')] == ""){
-            completed += key;
+            job_queue.push_back(key);
             cout << "Removing " << key << endl;
             uncompleted.erase(remove(uncompleted.begin(), uncompleted.end(), key), uncompleted.end());
             k--;
         }
     }
-    for(int i = 0; i < deps.size(); i++){
-        cout << char('A' + i) << " depends on " << deps[i] << endl;
-    }
-
-    cout << uncompleted.size() << endl;
-    while(uncompleted.size() != 0){
+    int time = 0;
+    while(completed.length() < 26){
+        //clear out any done jobs
+        for(auto &elf : elves){
+            if(elf.current_job != '.'){
+                if(time - elf.job_start >= int(elf.current_job - 'A') + 1 + bonus_time){
+                    //job is done
+                    completed += elf.current_job;
+                    elf.current_job = '.';
+                }
+            }
+        }
+        //Move ready jobs to job queue
         for(int i = 0; i < uncompleted.size(); i++){
             char key = uncompleted[i];
             bool unmet_dep = false;
             for(auto c : deps[key - 'A']){
-                if(find(uncompleted.begin(), uncompleted.end(), c) != uncompleted.end()){
+                if(completed.find(c) == string::npos){
                     unmet_dep = true;
                 }
             }
             if(!unmet_dep){
-                completed += key;
+                job_queue.push_back(key);
                 cout << "Removing " << key << endl;
                 uncompleted.erase(remove(uncompleted.begin(), uncompleted.end(), key), uncompleted.end());
                 i--;
             }
         }
+        //Assign jobs in queue to elf
+        for(int i = 0; i < 
+
+
+        time++;
     }
 
     cout << completed << endl;
     cout << "string length: " << completed.length() << endl;
+    cout << "Time: " << time << endl;
 
     return 0;
 }
