20f9d5ab14614484b59ebd4f8297cdda774939a9
Ethan Armbrust <EthanArmbrust@github.com>
2019-09-25 15:22:44 -0400

    day 8 part 1 done

--- aoc8.cpp
+++ aoc8.cpp
@@ -7,49 +7,52 @@ using namespace std;
 
 struct Node{
 	int id;
-	int size;
+	char letter;
+	int size = 0;
 	vector<Node> children;
 	vector<int> metadata;
 };
 
 int id_counter = 0;
 
-//Returns head node... eventually
 Node build_nodes(vector<int> numbers){
 	Node n;
 	n.id = id_counter++;
-	//We can get the size of a node by the size of it's children + size of metadata + 2
+	n.letter = n.id + 'A';
 	int child_count = numbers[0];
 	int metadata_count = numbers[1];
-	if(child_count == 0){
-		for(int i = 0; i < metadata_count; i++){
-			n.metadata.push_back(numbers[i+2]);
-		}
-		n.size = metadata_count + 2;
-	}
-	else{
-		for(int i = 0; i < child_count; i++){
-			int offset = 2;
-			for(auto c : n.children){
-				offset += c.size;
-			}
-			n.children.push_back(build_nodes(vector<int>(numbers.begin() + offset, numbers.end())));
-		}
+	for(int i = 0; i < child_count; i++){
+		int offset = 2;
 		for(auto c : n.children){
-			n.size += c.size;
+			offset += c.size;
 		}
-		for(int i = 0; i < metadata_count; i++){
-			n.metadata.push_back(numbers[i + n.size + 2]);
-		}
-		n.size += metadata_count + 2;
-		
+		n.children.push_back(build_nodes(vector<int>(numbers.begin() + offset, numbers.end())));
 	}
+	for(auto c : n.children){
+		n.size += c.size;
+	}
+	for(int i = 0; i < metadata_count; i++){
+		n.metadata.push_back(numbers[i + n.size + 2]);
+	}
+	n.size += metadata_count + 2;
+		
 	return n;
+}
+
+int meta_sum(Node head){
+	int sum = 0;
+	for(auto &n : head.children){
+		sum += meta_sum(n);
+	}
+	for(auto i : head.metadata){
+		sum += i;
+	}
+	return sum;
 }
 
 int main(){
 
-	ifstream infile("input/test8.txt");
+	ifstream infile("input/8.txt");
 	string s;
 	getline(infile, s);
 
@@ -61,6 +64,8 @@ int main(){
 	}
 
 	Node head = build_nodes(numbers);
+	int result = meta_sum(head);
+	cout << result << endl;
 
 
 	return 0;
